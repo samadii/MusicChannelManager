cc6d07445074d9d20e1b4af21884470a18cf95de
samadii <77415087+samadii@users.noreply.github.com>
2021-07-16 14:31:26 +0430

    Update main.py

--- main.py
+++ main.py
@@ -74,12 +74,12 @@ def change_caption(bot, update):
 
 
 def start(bot, update):
-    reply = "Hi !  I am Music Channel Manager Bot!  I can add a dynamic caption to the musics, just add me to a channel as admin and give me the permissions."
-    bot.send_message(chat_id = update.message.chat_id, text = reply)
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text='Hi !  I am Music Channel Manager Bot!\nI can add a dynamic caption to the musics, just add me to a channel as admin and give me the permissions.')
 
 
 handler = MessageHandler(Filters.audio, change_caption, channel_post_updates=True, message_updates=False)
-handlers = CommandHandler("start", start)
+handlers = CommandHandler('start', start)
 dispatcher.add_handler(handler=handler)
 dispatcher.add_handler(handler=handlers)
 
